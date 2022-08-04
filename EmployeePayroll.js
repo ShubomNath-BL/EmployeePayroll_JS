0ff4a15d18581be1879c5f259b44cd9d13be4ab0
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-04 13:30:53 +0530

    Refactore: Code is refactored

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -11,19 +11,23 @@ const fullTimeHours = 8;
 const wagePerHours = 20;
 let empHours = 0;
 let empCheck = Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case isPatrTime:
-        console.log("Employee is part time");
-        empHours = partTimeHours;
-        break;
-    case isFullTime:
-        console.log("Employee is full time");
-        empHours = fullTimeHours;
-        break;
-    default:
-        console.log("Employee is absent");
-        empHours = 0;
-        break;        
+//check if employee is part time, full time or absent.
+function getWorkHours(empCheck){
+    switch(empCheck){
+        case isPatrTime:
+            console.log("Employee is part time");
+            return partTimeHours;
+            break;
+        case isFullTime:
+            console.log("Employee is full time");
+            return fullTimeHours;
+            break;
+        default:
+            console.log("Employee is absent");
+            return 0;
+            break;        
+    }
 }
+empHours = getWorkHours(empCheck);
 let empWage = empHours * wagePerHours;
-console.log(empWage);
+console.log("Daily wage is: "+empWage);
