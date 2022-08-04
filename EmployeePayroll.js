f6343e2e7b50b627386ab980085219aefcc4c865
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-04 12:50:20 +0530

    Added: Employee attendence added

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,1 +1,8 @@
 console.log("Welcome to Employee payroll");
+const isAbsent = 0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck == isAbsent){
+    console.log("Employee is absent");
+}else{
+    console.log("Employee is present");
+}
