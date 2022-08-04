ee2343094cd593815241836e08dda5c20b74b048
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-04 13:44:49 +0530

    Added: Monthly wage calculation functionality added

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -2,6 +2,7 @@
 Below program to Calculate Daily Employee Wage based onpart time or full time work 
 - Use Math.Random to check absent, Part Time or Full Time.
 - Assume Part Time is 4 Hrs and Full time is 8 Hrs and per hour wage is $20.
+- Calculating Wages for a Month assuming 20 Working Days in a Month.
 */
 console.log("Welcome to Employee payroll");
 const isPatrTime = 1;
@@ -9,8 +10,8 @@ const isFullTime = 2;
 const partTimeHours = 4;
 const fullTimeHours = 8;
 const wagePerHours = 20;
+const totalWorkingDays = 20;
 let empHours = 0;
-let empCheck = Math.floor(Math.random()*10)%3;
 //check if employee is part time, full time or absent.
 function getWorkHours(empCheck){
     switch(empCheck){
@@ -28,6 +29,9 @@ function getWorkHours(empCheck){
             break;        
     }
 }
-empHours = getWorkHours(empCheck);
+for(let day = 0; day < totalWorkingDays; day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHours += getWorkHours(empCheck);
+}
 let empWage = empHours * wagePerHours;
-console.log("Daily wage is: "+empWage);
+console.log("Total wage is: "+empWage);
