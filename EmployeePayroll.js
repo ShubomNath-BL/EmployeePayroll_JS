2bc24cf5124cb6d999fe69815fede06fb9dd40d4
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-04 14:41:31 +0530

    Modified: Storing daily wages in an array

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -4,6 +4,7 @@ Below program to Calculate Daily Employee Wage based onpart time or full time wo
 - Assume Part Time is 4 Hrs and Full time is 8 Hrs and per hour wage is $20.
 - Calculate Wages till a condition of total working hours of 160 or max days of 20 is
 reached for a month.
+- Storing Daily Wage in a Array.
 */
 console.log("Welcome to Employee payroll");
 const isPatrTime = 1;
@@ -15,6 +16,8 @@ const totalWorkingDays = 20;
 const totalWorkingHours = 160;
 let empHours = 0;
 let workingDays = 0;
+let totalEmpHrs = 0;
+let empWageArray = new Array();
 //check if employee is part time, full time or absent.
 function getWorkHours(empCheck){
     switch(empCheck){
@@ -29,13 +32,19 @@ function getWorkHours(empCheck){
             break;        
     }
 }
+function calculateDailyWage(empHours){
+    return empHours*wagePerHours;
+}
 //Calculating total working days & working hours
 while(empHours<=totalWorkingHours && workingDays<totalWorkingDays){
     workingDays++;
     let empCheck = Math.floor(Math.random()*10)%3;
-    empHours += getWorkHours(empCheck);
+    empHours = getWorkHours(empCheck);
+    totalEmpHrs += empHours;
+    empWageArray.push(calculateDailyWage(empHours));
 }
-let empWage = empHours * wagePerHours;  //Calculating total wages.
+let empWage = calculateDailyWage(totalEmpHrs);  //Calculating total wages.
+console.log(empWageArray);
 console.log("Total wage is: "+empWage);
 console.log("Total Working Days: "+workingDays);
-console.log("Total Employee hours: "+empHours);
+console.log("Total Employee hours: "+totalEmpHrs);
